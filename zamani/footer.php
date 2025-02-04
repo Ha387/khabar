3b6854e7a8c6a2a90d5dde6b67b05ed35ba0b0eb
Ha387 <ha3an87.z@gmail.com>
2025-02-04 13:00:27 +0330

    Delete zamani directory

--- zamani/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <p>تمامی حقوق محفوظ است &copy; 2025</p>
-    </footer>
-
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
